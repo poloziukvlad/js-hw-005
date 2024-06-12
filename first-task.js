d4256d0c8f16b31e7670aa8a5f4f3157bbd4aa61
Poloziuk <polozettix@gmail.com>
2024-06-12 21:33:48 +0300

    added first task

---
+++ first-task.js
@@ -0,0 +1,12 @@
+// Типи даних:
+// Створіть змінну name і призначте їй ваше ім'я. Створіть змінну age і призначте їй ваш вік (ціле число). Створіть змінну isStudent і призначте їй значення true або false в залежності від того, чи ви студент. Створіть змінну height і призначте їй ваш зріст (дробове число). Виведіть всі ці значення в консоль, використовуючи console.log().
+
+const name = "Vlad";
+const age = 23;
+const isStudent = false;
+const height = 1.8;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(height);
