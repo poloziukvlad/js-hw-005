4a3a8cda06901127a59f0c8cf0f9dab54925dc44
Poloziuk <polozettix@gmail.com>
2024-06-12 22:02:55 +0300

    added fourth task

---
+++ fourth-task.js
@@ -0,0 +1,11 @@
+// Створіть об'єкт:
+// Створіть об'єкт person з властивостями name, age, isStudent та height, які отримають значення з ваших змінних. Виведіть вміст об'єкта person в консоль.
+
+const person = {
+  name: "Vlad",
+  age: 23,
+  isStudent: false,
+  height: 1.8,
+};
+
+console.log(person);
